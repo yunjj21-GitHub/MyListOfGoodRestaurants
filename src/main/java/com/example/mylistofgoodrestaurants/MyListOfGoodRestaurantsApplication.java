9ecc1b0a16204d34affb5f8c3b99af2436aa01bb
yunjj21-GitHub <yunjj21@hs.ac.kr>
2022-11-21 13:08:41 +0900

    initial commit

---
+++ src/main/java/com/example/mylistofgoodrestaurants/MyListOfGoodRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylistofgoodrestaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyListOfGoodRestaurantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyListOfGoodRestaurantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mylistofgoodrestaurants/MyListOfGoodRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylistofgoodrestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyListOfGoodRestaurantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
